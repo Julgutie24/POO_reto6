6d9c86ef6210955a54e2f5266f3caf3ffd41fee3
Julgutie24 <julgutierrezca@unal.edu.co>
2025-06-30 17:14:25 -0500

    Update point.py

--- Ejercicio_2_excepcions/Shape/point.py
+++ Ejercicio_2_excepcions/Shape/point.py
@@ -1,7 +1,11 @@
 import math
 
+from .exceptions import InvalidPointError
+
 class Point:
-    def __init__(self, x: int, y: int):
+    def __init__(self, x, y):
+        if not isinstance(x, (int, float)) or not isinstance(y, (int, float)):
+            raise InvalidPointError("Las coordenadas deben ser números.")
         self._x = x
         self._y = y
 
@@ -9,13 +13,18 @@ class Point:
         return self._x
 
     def set_x(self, value):
+        if not isinstance(value, (int, float)):
+            raise InvalidPointError("La coordenada X debe ser un número.")
         self._x = value
 
     def get_y(self):
         return self._y
 
     def set_y(self, value):
+        if not isinstance(value, (int, float)):
+            raise InvalidPointError("La coordenada Y debe ser un número.")
         self._y = value
 
     def compute_distance(self, other):
-        return math.hypot(self._x - other.get_x(), self._y - other.get_y())
+        from math import hypot
+        return hypot(self._x - other.get_x(), self._y - other.get_y())
